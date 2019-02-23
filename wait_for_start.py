a4624740cc664a5e43b03dafd71c358b8729d05b
perryrobotics <47753315+perryrobotics@users.noreply.github.com>
2019-02-22 20:36:50 -0600

    Improved calibration function

--- wait_for_start.py
+++ wait_for_start.py
@@ -3,25 +3,33 @@ import os, sys
 from wallaby import *
 
 def wait_for_start(port):      
-	print("light off.  Press A when ready!")
+	print("LIGHT OFF NOW!!!.  Press A when ready!")
  	msleep(2000)
-  	while a_button()==0:
+  	while not a_button():
 		off = analog(port)
 		print ("OFF: ",analog(port) )
  	print ("LIGHT OFF VALUE: ", off)
- 	           
-	print("light on.  Press B when ready!")
- 	msleep(2000)
-  	while b_button()==0:
+	if off <1500:
+		print ("BAD CALIBRATION!!!  DO NOT RUN!!!")
+		return 0
+ 	while a_button():
+		pass
+	
+	print("LIGHT ON NOW!!!.  Press B when ready!")
+  	while not b_button():
 		on = analog(port)
 		print ("ON: ",analog(port) )
  	print ("LIGHT ON VALUE: ", on)
+	if on >1000:
+		print ("BAD CALIBRATION!!!  DO NOT RUN!!!")
+		return 0
 
+	print("GOOD CALIBRATION!  LETS GO!!!")
 	print ("TURN LIGHT OFF NOW!!!! PRESS C WHEN READY")
   	while c_button()==0:
 		pass
    
-	thresh = (off -on)/2
+	thresh = (off + on)/2
 	while analog(port) > thresh:
 		print ("WAITING FOR LIGHT!!!")
 		pass
