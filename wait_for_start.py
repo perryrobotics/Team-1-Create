4c61f8601373771e070745a9efe58c356890b13b
perryrobotics <47753315+perryrobotics@users.noreply.github.com>
2019-02-23 08:57:46 -0600

    Update wait_for_start.py

--- wait_for_start.py
+++ wait_for_start.py
@@ -11,7 +11,7 @@ def wait_for_start(port):
  	print ("LIGHT OFF VALUE: ", off)
 	if off <1500:
 		print ("BAD CALIBRATION!!!  DO NOT RUN!!!")
-		return 0
+		return False
  	while a_button():
 		pass
 	
@@ -22,14 +22,21 @@ def wait_for_start(port):
  	print ("LIGHT ON VALUE: ", on)
 	if on >1000:
 		print ("BAD CALIBRATION!!!  DO NOT RUN!!!")
-		return 0
+		return False
 
-	print("GOOD CALIBRATION!  LETS GO!!!")
 	print ("TURN LIGHT OFF NOW!!!! PRESS C WHEN READY")
   	while c_button()==0:
 		pass
    
 	thresh = (off + on)/2
+	if thresh < 1000:
+		print "BAD CALIBRATION!!!!"
+		return False
+	
+	print("GOOD CALIBRATION!  LETS GO!!!")
+	print("ON VAL:",on)
+	print("OFF VAL: ", off)
+	print("THRESH: ", thresh)
 	while analog(port) > thresh:
 		print ("WAITING FOR LIGHT!!!")
 		pass
